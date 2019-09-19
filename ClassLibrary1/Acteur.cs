db64b80f39b07ddf9af77f345cd4755d5b9a0030
joni75014 <45243676+joni75014@users.noreply.github.com>
2019-09-19 16:32:30 +0200

    Ajoutez des fichiers projet.

---
+++ ClassLibrary1/Acteur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Acteur
+    {
+        public string NomActeur { get; set; }
+        public string PrenomActeur { get; set; }
+
+    }
+}
---
+++ ClassLibrary1/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Film
+    {
+        public int NbEntrees { get; set; }
+        public string TitreFilm { get; set; }
+    }
+    public void AjouterActeur (Acteur unActeur)
+    {
+        LesActeurs.Add(unActeur); 
+    }
+}
